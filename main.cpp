677d3e17ebecc933173c5c2a2a139ecdc6c4523d
Pletka Daniil <pletkadaniil@yandex.ru>
2024-05-16 16:41:00 +0300

    processing of the event ID 1. Customer arrived

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <set>
 
 struct client{
-    int time;
-    int table_num;
+    int table_num = -1;
+    int command = -1;
+    std::string time;
     std::string client_name;
-
 };
 
 bool only_digits_check(const std::string& line){
@@ -75,6 +76,12 @@ std::vector<std::string> split_line_by_spaces(const std::string& line){
     return data;
 }
 
+
+bool client_entry_valid_check(const std::string& time, const int& start_of_work, const int& end_of_work){
+    int minutes = time_to_minutes(time);
+    return minutes >= start_of_work && minutes <= end_of_work;
+}
+
 int main(int argc, char* argv[]){
     if (argc != 2){
         std::cerr << "Use " << argv[0] << " source destination" << std::endl;
@@ -89,9 +96,12 @@ int main(int argc, char* argv[]){
     
     try{
         std::string str;
+        int start_of_work = -1;
+        int end_of_work = -1;
         int count = 1;
         int total_number_of_tables = 0;
         int previous_time = -1;
+        std::vector<client> active_clients_data;
         while (getline(input_file, str)){
             std::vector<std::string> data = split_line_by_spaces(str);
             switch (count){
@@ -103,6 +113,8 @@ int main(int argc, char* argv[]){
                     }else{ throw std::string{"Format error in line: "}.append(str); }
                 case 2:
                     if (data.size() == 2 && time_valid_check(data[0]) && time_valid_check(data[1])){
+                        start_of_work = time_to_minutes(data[0]);
+                        end_of_work = time_to_minutes(data[1]);
                         ++count;
                         break;
                     }else{ throw std::string{"Format error in line: "}.append(str); }
@@ -118,21 +130,42 @@ int main(int argc, char* argv[]){
                             throw std::string{"Format error in line: "}.append(str);
                         }
                         previous_time = current_time;
+                        client new_client;
                         int number_of_command = std::stoi(data[1]);
                         switch (number_of_command){
-                            case 1: break;
+                            case 1: new_client = {-1, number_of_command, data[0], data[2]}; break;
                             case 2:
                                 if (data.size() == 4 && table_number_range_check(data[3], total_number_of_tables)){
+                                    new_client = {std::stoi(data[3]), number_of_command,data[0], data[2]};
                                     break;
                                 }else{throw std::string{"Format error in line: "}.append(str);}
-                            case 3: break;
-                            case 4: break;
+                            case 3: new_client = {-1, number_of_command, data[0], data[2]}; break;
+                            case 4: new_client = {-1, number_of_command, data[0], data[2]}; break;
                         }
+                        active_clients_data.push_back(new_client);
                         break;
                     }else{ throw std::string{"Format error in line: "}.append(str); }   
-                // default:
-                //     break;
             }
+        }
+        std::set<std::string> clients_in_club;
+        for (const auto& client: active_clients_data) {
+            switch (client.command){
+                case 1:
+                    std::cout << client.time << " " << client.command << " " << client.client_name << std::endl;
+                    if(!client_entry_valid_check(client.time, start_of_work, end_of_work)){
+                        std::cout << client.time << " 13 NotOpenYet" << std::endl;
+                        break;
+                    }else if (clients_in_club.count(client.client_name)){
+                        std::cout << client.time << " 13 YouShallNotPass" << std::endl;
+                        break;  
+                    }
+                    clients_in_club.insert(client.client_name);
+                    break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+            }
+            // std::cout << client.time << " " << client.command << " " << client.client_name << " " << client.table_num << "\n";
         }
     }catch (const std::string& error_message){
         std::cout << error_message << std::endl;
