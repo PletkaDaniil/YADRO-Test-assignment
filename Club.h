d0bf7774d3626cc75eb71b0e98f00cecb5e446cd
Pletka Daniil <pletkadaniil@yandex.ru>
2024-05-18 03:07:29 +0300

    let's create a class to combine all the implemented functions

---
+++ Club.h
@@ -0,0 +1,29 @@
+#ifndef CLUB_H
+#define CLUB_H
+
+#include "core_functions.h"
+
+class Club{
+private:
+    int start_of_work = -1;
+    int end_of_work = -1;
+    int check_stage = 1;
+    int total_number_of_tables = 0;
+    int previous_time = -1;
+    int table_price = 0;
+
+    std::map <int, bool> table_availability;
+    std::vector<client_info> active_clients_data;
+public:
+
+    void file_correctness(const std::string& current_file_line){
+        ensure_data_correctness(current_file_line, check_stage, total_number_of_tables, start_of_work, end_of_work, table_price, previous_time, table_availability, active_clients_data);
+    }
+
+    void club_working(){
+        process_club_work_and_handle_clients(active_clients_data, table_availability, start_of_work, end_of_work, table_price, total_number_of_tables); 
+    }
+
+};
+
+#endif
--- club_management.cpp
+++ club_management.cpp
@@ -1,28 +1,19 @@
 #include <fstream>
-#include "core_functions.h"
+#include "Club.h"
 
 int main(int argc, char* argv[]){
     check_args(argc, argv);
-
     std::ifstream input_file;
     open_input_file(input_file, argv);
+    Club computer_club;
     try{
-        std::string current_file_line;
-        int start_of_work = -1;
-        int end_of_work = -1;
-        int check_stage = 1;
-        int total_number_of_tables = 0;
-        int previous_time = -1;
-        int table_price = 0;
-
-        std::map <int, bool> table_availability;
-        std::vector<client_info> active_clients_data;
-
         // Reading the input file information (and checking it)
+        std::string current_file_line;
         while (getline(input_file, current_file_line)){
-            ensure_data_correctness(current_file_line, check_stage, total_number_of_tables, start_of_work, end_of_work, table_price, previous_time, table_availability, active_clients_data);
+            computer_club.file_correctness(current_file_line);
         }
-        process_club_work_and_handle_clients(active_clients_data, table_availability, start_of_work, end_of_work, table_price, total_number_of_tables);   
+        computer_club.club_working(); 
+
     }catch (const std::string& error_message){
         std::cerr << error_message << "\n";
     }
